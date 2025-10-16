131623d05152eefcf34d799cd546f8c6bbff9418
Kirill Sakun <kirillsakun03@gmail.com>
2025-10-16 22:22:06 +0200

    [CLIENT-0] Feature: Add router

---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import styles from './HomePage.module.css'
+
+export const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Food Delivery Platform</h1>
+      <p>Welcome to our food delivery service</p>
+    </div>
+  )
+}
