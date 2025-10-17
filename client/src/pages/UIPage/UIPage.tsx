e7d30acefcaa22d00d86a4a51b70c460a425158b
Kirill Sakun <kirillsakun03@gmail.com>
2025-10-17 23:20:49 +0200

    [CLIENT-0] Refactor: Adjust base UI components props

--- client/src/pages/UIPage/UIPage.tsx
+++ client/src/pages/UIPage/UIPage.tsx
@@ -1,23 +1,29 @@
-import { UIButton, UIContainer, UISection, UIFlex } from '../../shared/ui'
+import { UIButton, UIContainer, UISection, UIFlex, UIMain } from '../../shared/ui'
 
 export const UIPage = () => {
-  return (
-    <UIContainer>
-      <h1>UI Components</h1>
+	return (
+		<UIMain>
+			<UISection>
+				<UIContainer>
+					<h1>UI Components</h1>
+				</UIContainer>
+			</UISection>
+			<UISection variant={"secondary"}>
+				<UIContainer>
+					<h2>Buttons</h2>
+					<UIFlex gap="lg" wrap>
+						<UIButton variant="solid" colorType="primary">Primary Solid</UIButton>
+						<UIButton variant="solid" colorType="secondary">Secondary Solid</UIButton>
+						<UIButton variant="solid" colorType="danger">Danger Solid</UIButton>
 
-      <UISection title="Buttons">
-        <UIFlex gap="lg" wrap>
-          <UIButton variant="solid" colorType="primary">Primary Solid</UIButton>
-          <UIButton variant="solid" colorType="secondary">Secondary Solid</UIButton>
-          <UIButton variant="solid" colorType="danger">Danger Solid</UIButton>
+						<UIButton variant="outline" colorType="primary">Primary Outline</UIButton>
+						<UIButton variant="outline" colorType="secondary">Secondary Outline</UIButton>
+						<UIButton variant="outline" colorType="danger">Danger Outline</UIButton>
 
-          <UIButton variant="outline" colorType="primary">Primary Outline</UIButton>
-          <UIButton variant="outline" colorType="secondary">Secondary Outline</UIButton>
-          <UIButton variant="outline" colorType="danger">Danger Outline</UIButton>
-
-          <UIButton variant="solid" colorType="primary" disabled>Disabled</UIButton>
-        </UIFlex>
-      </UISection>
-    </UIContainer>
-  )
+						<UIButton variant="solid" colorType="primary" disabled>Disabled</UIButton>
+					</UIFlex>
+				</UIContainer>
+			</UISection>
+		</UIMain>
+	)
 }
---
+++ client/src/shared/ui/UIMain/UIMain.tsx
@@ -0,0 +1,15 @@
+import type { FC, ReactNode } from 'react'
+import styles from './UIMain.module.css'
+
+interface UIMainProps {
+  children: ReactNode
+  className?: string
+}
+
+export const UIMain: FC<UIMainProps> = ({ children, className = '' }) => {
+  return (
+    <main className={`${styles.main} ${className}`}>
+      {children}
+    </main>
+  )
+}
